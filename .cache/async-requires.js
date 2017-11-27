e17a07e1c95a54f8af11873e49e019ed8e1bfb53
rahid <hidayat.85@gmail.com>
2017-11-28 05:37:35 +0700

    install gatsby

---
+++ .cache/async-requires.js
@@ -0,0 +1,26 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+exports.components = {
+  "component---cache-dev-404-page-js": require("gatsby-module-loader?name=component---cache-dev-404-page-js!/home/rahid/project-rahid/gatsby-site/.cache/dev-404-page.js"),
+  "component---src-pages-404-js": require("gatsby-module-loader?name=component---src-pages-404-js!/home/rahid/project-rahid/gatsby-site/src/pages/404.js"),
+  "component---src-pages-index-js": require("gatsby-module-loader?name=component---src-pages-index-js!/home/rahid/project-rahid/gatsby-site/src/pages/index.js"),
+  "component---src-pages-page-2-js": require("gatsby-module-loader?name=component---src-pages-page-2-js!/home/rahid/project-rahid/gatsby-site/src/pages/page-2.js")
+}
+
+exports.json = {
+  "layout-index.json": require("gatsby-module-loader?name=path---!/home/rahid/project-rahid/gatsby-site/.cache/json/layout-index.json"),
+  "dev-404-page.json": require("gatsby-module-loader?name=path---dev-404-page!/home/rahid/project-rahid/gatsby-site/.cache/json/dev-404-page.json"),
+  "layout-index.json": require("gatsby-module-loader?name=path---!/home/rahid/project-rahid/gatsby-site/.cache/json/layout-index.json"),
+  "404.json": require("gatsby-module-loader?name=path---404!/home/rahid/project-rahid/gatsby-site/.cache/json/404.json"),
+  "layout-index.json": require("gatsby-module-loader?name=path---!/home/rahid/project-rahid/gatsby-site/.cache/json/layout-index.json"),
+  "index.json": require("gatsby-module-loader?name=path---index!/home/rahid/project-rahid/gatsby-site/.cache/json/index.json"),
+  "layout-index.json": require("gatsby-module-loader?name=path---!/home/rahid/project-rahid/gatsby-site/.cache/json/layout-index.json"),
+  "page-2.json": require("gatsby-module-loader?name=path---page-2!/home/rahid/project-rahid/gatsby-site/.cache/json/page-2.json"),
+  "layout-index.json": require("gatsby-module-loader?name=path---!/home/rahid/project-rahid/gatsby-site/.cache/json/layout-index.json"),
+  "404-html.json": require("gatsby-module-loader?name=path---404-html!/home/rahid/project-rahid/gatsby-site/.cache/json/404-html.json")
+}
+
+exports.layouts = {
+  "index": require("gatsby-module-loader?name=component---src-layouts-index-js!/home/rahid/project-rahid/gatsby-site/.cache/layouts/index.js")
+}
---
+++ .cache/layouts/index.js
@@ -0,0 +1,7 @@
+
+  import React from "react"
+  import Component from "/home/rahid/project-rahid/gatsby-site/src/layouts/index.js"
+  import data from "/home/rahid/project-rahid/gatsby-site/.cache/json/layout-index.json"
+
+  export default (props) => <Component {...props} {...data} />
+  
